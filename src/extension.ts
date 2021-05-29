73fb5b6474517257baf747ef6a7b9323e0c7456f
Abdelrhman <abdelrhman.ibrahim101@gmail.com>
2021-05-29 19:15:57 +0200

    fix: handle if last condition is an if else

--- src/extension.ts
+++ src/extension.ts
@@ -4,17 +4,17 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-function findClosingBracketMatchIndex(str: string | any[], pos:  number) {
-	if (str[pos] !== '{') {
-	  throw new Error("No '{' at index " + pos);
+function findClosingBracketMatchIndex(opening:string,closing:string,str: string | any[], pos:  number) {
+	if (str[pos] !== opening) {
+	  throw new Error(`No ${opening} at index ` + pos);
 	}
 	let depth = 1;
 	for (let i = pos + 1; i < str.length; i++) {
 	  switch (str[i]) {
-	  case '{':
+	  case opening:
 		depth++;
 		break;
-	  case '}':
+	  case closing:
 		if (--depth === 0) {
 		  return i;
 		}
@@ -38,13 +38,30 @@ export function activate(context: vscode.ExtensionContext) {
 			const statements = document.getText(selection);
 			let lastElse = statements.indexOf('else {');
 			//get the opening bracket index
-			lastElse+=5;
-			//find the last else enclosing bracket index
-			const enclosingBracket = findClosingBracketMatchIndex(statements,lastElse);
+			let changedIfElse=statements
+			if(lastElse>0){
+				
+				lastElse+=5;
+				//find the last else enclosing bracket index
+			const enclosingBracket = findClosingBracketMatchIndex('{','}',statements,lastElse);
 			//replacing last else enclosing bracket with return;
 			let removedLastBracket = statements.substring(0, enclosingBracket) + 'return;' + statements.substring(enclosingBracket+1);
-			//replacing last else with return
-			let changedIfElse = removedLastBracket.replace(/} else {/g, `return;}\n`);
+		//replacing last else with return
+		 changedIfElse = removedLastBracket.replace(/} else {/g, `return;}\n`);	
+		}
+		if(lastElse<=0){
+			let lastIfElse =  statements.lastIndexOf('else if (')
+			console.log("lastIfElse",lastIfElse);
+			lastIfElse+=8
+			let lastIfElsePracketIndex = findClosingBracketMatchIndex('(',')',statements,lastIfElse)
+			 lastIfElsePracketIndex+=2
+			let lastCurly = findClosingBracketMatchIndex('{','}',statements,lastIfElsePracketIndex)
+
+			changedIfElse = statements.substring(0, lastCurly) + 'return;' + statements.substring(lastCurly+1);
+			
+			
+		}
+			
 			//replacing all remaining else if with return
 			changedIfElse = changedIfElse.replace(/} else/g, `return;}\n`);
 			editor.edit(editBuilder => {
